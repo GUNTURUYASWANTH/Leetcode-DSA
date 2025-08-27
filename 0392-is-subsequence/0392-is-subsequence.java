14a3ead34da256c0ea005370210acba29737d42b
gunturu yaswanth <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2025-08-27 12:33:25 +0530

    Time: 1 ms (93.84%), Space: 41.7 MB (23.91%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -9,20 +9,39 @@ class Solution {
             return false;
          }
     }
-    public static boolean check (String s1, String s2 , int n ,int m)
+    public static boolean check(String s1, String s2 , int n , int m)
     {
-        if(n==0)
+        int i=0;
+        int j=0;
+        while(i<n && j<m)
         {
-            return true;
+            if(s1.charAt(i) == s2.charAt(j))
+            {
+                i++;
+            }
+            j++;
         }
-        if(m==0)
-        {
-            return false;
-        }
-        if(s1.charAt(n-1)==s2.charAt(m-1))
-        {
-            return check(s1,s2,n-1,m-1);
-        }
-        return check(s1,s2,n,m-1);
+        return i==n;
     }
+    
 }
+
+
+// Recursive Approach
+
+// public static boolean check (String s1, String s2 , int n ,int m)
+//     {
+//         if(n==0)
+//         {
+//             return true;
+//         }
+//         if(m==0)
+//         {
+//             return false;
+//         }
+//         if(s1.charAt(n-1)==s2.charAt(m-1))
+//         {
+//             return check(s1,s2,n-1,m-1);
+//         }
+//         return check(s1,s2,n,m-1);
+//     }
