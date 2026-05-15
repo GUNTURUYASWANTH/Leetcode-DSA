2f5f9b3d11f5bf3f0892baaa4891c453eaa1b0ae
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-15 07:50:34 +0530

    Time: 1 ms (88.88%), Space: 44.5 MB (7.25%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int totalSubsets = 1 << n;
+        List<List<Integer>> order = new ArrayList<List<Integer>>();
+        for(int i=0 ; i < totalSubsets ; i++)
+        {
+            List<Integer> subsets = new ArrayList<Integer>();
+            for(int j=0;j<n;j++)
+            {
+                if((i & (1 << j)) != 0)
+                {
+                    subsets.add(nums[j]);
+                }
+            }
+            order.add(subsets);
+        }
+        return order;
+    }
+}
