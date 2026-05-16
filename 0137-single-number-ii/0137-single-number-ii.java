7ab0a48f6e4c5fd7ac94e658d2bb5b79a832e9da
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-16 16:23:18 +0530

    Time: 7 ms (19.96%), Space: 45.5 MB (34.74%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -4,16 +4,13 @@ class Solution {
         int ans = -1;
         for(int i=1;i<nums.length;i+=3)
         {
-            if(nums[i] != nums[i-1] || nums[i] != nums[i+1])
+            if(nums[i] != nums[i-1])
             {
                 if(nums[i] != nums[i-1])
                 {
                     ans = nums[i-1];
                 }
-                else
-                {
-                    ans = nums[i+1];
-                }
+                
                 break;
             }
         }
