d69909f1d44e8ced01143cacc8cfa004ac4b8663
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-16 16:18:34 +0530

    Time: 7 ms (19.96%), Space: 45.3 MB (49.02%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int ans = -1;
+        for(int i=1;i<nums.length;i+=3)
+        {
+            if(nums[i] != nums[i-1] || nums[i] != nums[i+1])
+            {
+                if(nums[i] != nums[i-1])
+                {
+                    ans = nums[i-1];
+                }
+                else
+                {
+                    ans = nums[i+1];
+                }
+                break;
+            }
+        }
+        if(ans == -1)
+        {
+            ans = nums[nums.length-1];
+        }
+
+        return ans;
+    }
+}
