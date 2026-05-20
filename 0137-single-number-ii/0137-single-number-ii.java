6301c6c776cf5e7142763fb33ff047b726d7a04a
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-20 13:55:20 +0530

    Time: 8 ms (11.81%), Space: 45.6 MB (32.8%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,24 +1,38 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-        int ans = -1;
-        for(int i=1;i<nums.length;i+=3)
-        {
-            if(nums[i] != nums[i-1])
-            {
-                if(nums[i] != nums[i-1])
-                {
-                    ans = nums[i-1];
-                }
-                
-                break;
-            }
-        }
-        if(ans == -1)
-        {
-            ans = nums[nums.length-1];
-        }
+        int one = 0;
+        int two = 0;
 
-        return ans;
+        for(int i=0;i<nums.length;i++)
+        {
+            one = (one ^ nums[i]) & (~two);
+            two = (two ^ nums[i]) & (~one);
+        }
+        return one;
+        
     }
 }
+
+
+// this method is like make a array sequence can be used but taking more time 
+
+// int ans = -1;
+//         for(int i=1;i<nums.length;i+=3)
+//         {
+//             if(nums[i] != nums[i-1])
+//             {
+//                 if(nums[i] != nums[i-1])
+//                 {
+//                     ans = nums[i-1];
+//                 }
+                
+//                 break;
+//             }
+//         }
+//         if(ans == -1)
+//         {
+//             ans = nums[nums.length-1];
+//         }
+
+            // return ans;
