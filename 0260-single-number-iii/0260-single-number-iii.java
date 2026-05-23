6cb6a021cb4878f3c991debaf1200bf9267af116
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-23 19:16:52 +0530

    Time: 96 ms (6.57%), Space: 47.7 MB (87.56%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int x = Arrays.stream(nums).reduce(0,(result,i)->result ^ i);
+        int rightMost = (x & (x-1)) ^ x; // Which give the set value from last to first
+        int b1 = 0;
+        int b2 = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if((nums[i] & rightMost) != 0)
+            {
+                b1 = b1 ^ nums[i];
+                continue;
+            }
+            b2 = b2 ^ nums[i];
+            
+        }
+
+        return new int[]{b1,b2};
+    }
+}
