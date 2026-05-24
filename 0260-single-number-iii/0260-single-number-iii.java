7055b7785859d244463ca3b829906c2fa41d5c48
GUNTURU YASWANTH <118712918+GUNTURUYASWANTH@users.noreply.github.com>
2026-05-24 12:32:30 +0530

    Time: 3 ms (35.37%), Space: 48.2 MB (21.62%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int x = Arrays.stream(nums).reduce(0,(result,i)->result ^ i);
-        int rightMost = (x & (x-1)) ^ x; // Which give the set value from last to first
+        int rightMost = x & (-x); // Which give the set value from last to first
         int b1 = 0;
         int b2 = 0;
         for(int i=0;i<nums.length;i++)
